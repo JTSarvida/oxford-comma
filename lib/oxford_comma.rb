cae9fc21aae75434622a60ad205239d3d0da40e8
jtsarvida <jtsarvida@gmail.com>
2022-07-02 03:12:16 -0400

    completed assignment

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,20 @@
 def oxford_comma(array)
-
+    oxford_array = []
+    if array.size == 1
+        oxford_array = array.join
+        return oxford_array
+    elsif array.size == 2
+        oxford_array = array.join(' and ')
+        return oxford_array
+    elsif array.size == 3
+        oxford_array = array[0..1].join(', ')
+        oxford_array << ", and #{array[2]}"
+        return oxford_array
+    elsif array.size > 3
+        last_index = array.size-1
+        oxford_array = array[0..last_index-1].join(', ')
+        oxford_array << ", and #{array[last_index]}"
+        return oxford_array
+    end
+    
 end
